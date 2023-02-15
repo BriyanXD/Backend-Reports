2d5b2688316eddfb26aad1887964f731d1600f03
briyanxd <briyana1997@gmail.com>
2023-02-15 13:42:28 -0400

    changes

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 
-const PORT_APP = process.env.PORT_APP || 3001;
+const PORT_APP = process.env.PORT_APP || "3001";
 const HOST_APP = process.env.HOST_APP || "192.167.0.9";
 const USER_DB = process.env.USER_DB || "postgres";
-const PASS_DB = process.env.PASS_DB || 123456789;
-const HOST_DB = process.env.HOST_DB || "reports_db";
-const PORT_DB = process.env.PORT_DB || 5432;
+const PASS_DB = process.env.PASS_DB || "123456789";
+const HOST_DB = process.env.HOST_DB || "localhost";
+const PORT_DB = process.env.PORT_DB || "5432";
 const NAME_DB = process.env.NAME_DB || "reports";
 
 
